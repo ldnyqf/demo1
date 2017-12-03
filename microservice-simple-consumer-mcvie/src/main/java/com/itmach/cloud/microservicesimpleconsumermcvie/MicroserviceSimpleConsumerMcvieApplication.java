cd627b161ecacb038841aa1bce6c799b4de457eb
ldnyqf <32232341+ldnyqf@users.noreply.github.com>
2017-12-03 23:04:40 +0800

    Add files via upload

---
+++ microservice-simple-consumer-mcvie/src/main/java/com/itmach/cloud/microservicesimpleconsumermcvie/MicroserviceSimpleConsumerMcvieApplication.java
@@ -0,0 +1,12 @@
+package com.itmach.cloud.microservicesimpleconsumermcvie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimpleConsumerMcvieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimpleConsumerMcvieApplication.class, args);
+	}
+}
